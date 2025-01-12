bc62dc09adbd68a9e91bdb0e610afa073f067c7f
tellgeniewish <jadastras2@gmail.com>
2025-01-12 15:45:13 +0900

    :sparkles: 메인 화면 버튼 구현

--- pharmquest/src/components/layout/Header.tsx
+++ pharmquest/src/components/layout/Header.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => {
   return (
     <>
-      <div className="w-full h-[102px] shrink-0 bg-white justify-center items-center flex box-border gap-2.5 border-b border-solid border-gray-100">
+      <div className="w-full h-[102px] shrink-0 justify-center items-center flex box-border gap-2.5">
         <button className='ml-[262px] my-7 flex w-[155px] h-[46px] py-2 px-10 shrink-0 bg-gray-100 text-[20px] font-semibold'>로고 타입</button>
         <span className='flex ml-[52px] mr-[260px] w-[739px] h-[46px] py-2 cursor-pointer text-headline-m my-7 items-center m-auto'>
           <button className='w-[141px]'>상비약 리스트</button>
--- pharmquest/src/pages/index.tsx
+++ pharmquest/src/pages/index.tsx
@@ -1,6 +1,27 @@
+import Header from '@/components/layout/Header'
+import Footer from '@/components/layout/Footer';
+
 export default function Home() {
   return (
     <>
+      <div className='h-[1006px] bg-green-100'>
+        <Header/>
+        <div className="w-[293px] h-[171px] pl-5 py-6 bg-[#138e5d] rounded-xl flex-col justify-start items-start gap-[45px] inline-flex">
+    <div className="justify-start items-center gap-2 inline-flex">
+        <div className="text-white text-xl font-bold font-['Pretendard Variable'] leading-[30px]">상비약 리스트</div>
+    </div>
+    <div className="text-white text-base font-normal font-['Pretendard Variable'] leading-normal">증상 및 약 이름을 검색하고<br/>필요한 상비약 정보를 얻을 수 있어요.</div>
+</div>
+        <button className='flex w-[293px] h-[171px] py-6 px-2 flex-col items-start gap-[45px] rounded-xl bg-green-500 mt-[128px] ml-[260px]'>
+          <div className='flex items-center gap-2 text-white mt-6 text-headline-b'>
+            <div>상비약 리스트</div>
+            <div>&gt;</div>
+          </div>
+          <div className="text-white text-base font-normal font-['Pretendard Variable'] leading-normal">증상 및 약 이름을 검색하고<br/>필요한 상비약 정보를 얻을 수 있어요.</div>
+          
+        </button>
+      </div>
+      <Footer/>
     </>
   );
 }
