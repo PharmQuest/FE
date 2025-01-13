8e1078a5ee8b2f8fc6a60fd791c3d71c1a1f88c2
jjjuni <0210leejun@gmail.com>
2025-01-13 20:31:22 +0900

    💄 상비약 컴포넌트 수정

--- src/components/common/MedicineCard.tsx
+++ src/components/common/MedicineCard.tsx
@@ -6,7 +6,7 @@ const MedicineCard = () => {
 
   return (
     // medicine-card 피그마에는 border-color: gray-100으로 설정되어 있지만 보이지 않아서 임시로 설정
-    <div className={`flex p-5 border-gray-200 border rounded-lg w-[450px] h-[178px] items-center hover:cursor-pointer`} onClick={() => router.push('medicines/1')}>
+    <div className={`flex p-5 border-gray-200 border rounded-lg h-[178px] items-center hover:cursor-pointer`} onClick={() => router.push('medicines/1')}>
       {/* medicine-image */}
       <div className={`w-[138px] h-[138px] bg-[#D9D9D9] rounded`}>
         {/* <img src='url' className={`w-[138px] h-[138px]`}>
