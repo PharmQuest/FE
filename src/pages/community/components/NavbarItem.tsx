8bf76e5d69b82197da4abca652e8c0b9b2e73e84
hyesngy <tell280210@gmail.com>
2025-01-15 15:21:41 +0900

    ♻️ NavbarItem 및 PopularPostItem svg 리팩토링

--- src/pages/community/components/NavbarItem.tsx
+++ src/pages/community/components/NavbarItem.tsx
@@ -1,3 +1,4 @@
+import { GlobeIcon } from "@public/svgs";
 import React from "react";
 
 interface NavbarItemProps {
@@ -7,7 +8,7 @@ interface NavbarItemProps {
 const NavbarItem: React.FC<NavbarItemProps> = ({ text }) => {
   return (
     <div className="flex flex-row w-full px-4 py-2 gap-2 text-subhead2-sb text-gray-400 border-solid border-t-[1px] cursor-pointer">
-      <img src="./svgs/globe.svg" alt="glove" />
+      <GlobeIcon />
       {text}
     </div>
   );
--- src/pages/community/components/PopularPostItem.tsx
+++ src/pages/community/components/PopularPostItem.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import SubjectTag from "./SubjectTag";
 import BestTag from "./BestTag";
+import { CommentIcon, LikeIcon, ScrapIcon } from "@public/svgs";
 
 interface PopularPostItemProps {
   subject: string; // 주제
@@ -31,13 +32,19 @@ const PopularPostItem: React.FC<PopularPostItemProps> = ({
       <p className="text-body1-r text-gray-500">{content}</p>
       <div className="flex flex-row justify-between text-body2-r text-gray-400">
         <p>{date}</p>
-        <div className="flex flex-row gap-[2px]">
-          <img src="/svgs/like.svg" alt="like" />
-          {likes}
-          <img src="/svgs/comment.svg" alt="comment" className="ml-2" />
-          {comments}
-          <img src="/svgs/scrap.svg" alt="scrap" className="ml-2" />
-          {scraps}
+        <div className="flex flex-row gap-[10px]">
+          <div className="flex flex-row gap-[2px]">
+            <LikeIcon className="cursor-pointer" />
+            {likes}
+          </div>
+          <div className="flex flex-row gap-[2px]">
+            <CommentIcon className="cursor-pointer" />
+            {comments}
+          </div>
+          <div className="flex flex-row gap-[2px]">
+            <ScrapIcon className="cursor-pointer" />
+            {scraps}
+          </div>
         </div>
       </div>
     </div>
