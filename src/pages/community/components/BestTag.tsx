9535717e6cf3037ba71b1703b8ffa71f7c9de4a8
hyesngy <tell280210@gmail.com>
2025-01-16 13:36:13 +0900

    💄 BestTag style 개선

--- src/pages/community/components/BestTag.tsx
+++ src/pages/community/components/BestTag.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const BestTag = () => {
   return (
-    <div className="w-fit px-[6px] py-[2px] rounded-[1000px] border-2 border-solid border-point text-subhead3-sb text-point">
+    <div className="w-fit px-[6px] rounded-[1000px] border-2 border-solid border-point text-subhead3-sb text-point flex self-center">
       BEST
     </div>
   );
