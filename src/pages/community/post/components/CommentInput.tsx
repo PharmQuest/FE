fd8f9149e2f16caf6a4642e8fd8773ab2d68b399
hyesngy <tell280210@gmail.com>
2025-01-17 13:17:39 +0900

    ♻️ GrayButton 컴포넌트화 및 분리

--- src/pages/community/post/components/CommentInput.tsx
+++ src/pages/community/post/components/CommentInput.tsx
@@ -1,6 +1,11 @@
 import React from "react";
+import GrayButton from "../../components/GrayButton";
 
 const CommentInput = () => {
+  const handleClick = () => {
+    // 등록 이벤트 처리
+  };
+
   return (
     <div className="flex flex-col gap-2 w-full pt-2 pb-3 pr-3 pl-4 rounded border border-solid border-gray-200">
       <p className="text-subhead1-sb text-gray-600">닉네임닉네임</p>
@@ -10,11 +15,7 @@ const CommentInput = () => {
       />
       <div className="flex flex-row gap-3 justify-end">
         <span className="text-body1-r text-gray-300">0/3000</span>
-        <input
-          type="button"
-          value="등록"
-          className="w-[49px] h-29px] text-center text-subhead2-sb text-gray-400 bg-gray-100 rounded cursor-pointer"
-        ></input>
+        <GrayButton text="등록" onClick={handleClick} />
       </div>
     </div>
   );
