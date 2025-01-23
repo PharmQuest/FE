82fd3c9f5939444d8e7ec8e960793fb0a031828b
jjjuni <0210leejun@gmail.com>
2025-01-24 00:11:05 +0900

    ♻️ 토큰 환경변수 처리

--- src/pages/community/apis/axios-instance.ts
+++ src/pages/community/apis/axios-instance.ts
@@ -1,10 +1,9 @@
 import axios from "axios";
 
-
 const axiosPOSTInstance = axios.create({
   headers: {
     // 임의로 토큰값 넣어서 개발...
-    Authorization: `Bearer 토큰`
+    Authorization: `Bearer ${process.env.NEXT_PUBLIC_ACCESS_TOKEN}`
     
     // Authorization: `Bearer ${localStorage.getItem('accessToken')}` 로그인 개발 되면 이렇게 수정
   }
