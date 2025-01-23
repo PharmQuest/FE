3dc585e618793de74811d67420c9958f20468207
jjjuni <0210leejun@gmail.com>
2025-01-23 20:09:09 +0900

    ♻️ Search 컴포넌트 리팩토링링

--- src/components/common/Search.tsx
+++ src/components/common/Search.tsx
@@ -14,6 +14,7 @@ const Search: React.FC<SearchProps> = ({
 
   const router = useRouter();
   const pathName = router.pathname;
+  const isHomePage = pathName === '/' ? true : false;
 
   const [searchText, setSearchText] = useState("");
 
@@ -25,18 +26,18 @@ const Search: React.FC<SearchProps> = ({
       )}
 
       {/* 검색창 */}
-      <div className={`flex items-center bg-white rounded-full flex-grow shadow-md ${pathName === '/' ? `h-[54px] px-6 py-3 gap-5` : `h-10 px-4 py-2 gap-4`} `}>
+      <div className={`flex items-center bg-white rounded-full flex-grow shadow-md ${isHomePage ? `h-[54px] px-6 py-3 gap-5` : `h-10 px-4 py-2 gap-4`} `}>
         {/* 위치선택 */}
         <button className={`flex items-center gap-2 bg-none rounded-full text-gray-500`}>
-          <MapPinIcon className={`${pathName === '/' ? `w-7` : `w-5`}`} />
-          <p className={`${pathName === '/' ? `w-[35px] text-headline-m` : `w-7 text-body1-r`}`}>{countryLabel}</p>
+          <MapPinIcon className={`${isHomePage ? `w-7` : `w-5`}`} />
+          <p className={`${isHomePage ? `w-[35px] text-headline-m` : `w-7 text-body1-r`}`}>{countryLabel}</p>
         </button>
-        <SearchBar className={`${pathName === '/' ? `h-[22px]` : `h-4`}`}/>
+        <SearchBar className={`${isHomePage ? `h-[22px]` : `h-4`}`}/>
         <div className={`flex gap-2 grow`}>
-          <SearchIcon className={`${pathName === '/' ? `w-[30px]` : `w-6`}`} />
+          <SearchIcon className={`${isHomePage ? `w-[30px]` : `w-6`}`} />
           <input
             type="text"
-            className={`grow bg-transparent text-gray-600 placeholder-gray-300 focus:outline-none ${pathName === '/' ? `text-headline-m` : `text-body1-r`}`}
+            className={`grow bg-transparent text-gray-600 placeholder-gray-300 focus:outline-none ${isHomePage ? `text-headline-m` : `text-body1-r`}`}
             placeholder="복통약"
             value={searchText}
             onChange={(e) => setSearchText(e.target.value)}
@@ -44,7 +45,7 @@ const Search: React.FC<SearchProps> = ({
         </div>
         {searchText &&
           <XIcon
-            className={`cursor-pointer ${pathName === '/' ? `w-5` : `w-4 mr-1`}`}
+            className={`cursor-pointer ${isHomePage ? `w-5` : `w-4 mr-1`}`}
             onClick={() => setSearchText('')} />
         }
       </div>
