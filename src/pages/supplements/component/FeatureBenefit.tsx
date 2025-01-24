943719bf8036dc26a3d0dbd06e8bc215bec19f7c
9aeun <rlarkdms0424@naver.com>
2025-01-24 11:03:42 +0900

    :sparkles: 헤더 컴포넌트 구현

---
+++ src/pages/supplements/component/FeatureBenefit.tsx
@@ -0,0 +1,38 @@
+import CheckCircle from "@public/assets/icons/check-circle.svg";
+
+interface FeatureBenefitProps {
+    features: string[];
+    benefits: string[];
+}
+
+export default function FeatureBenefit({ features, benefits }: FeatureBenefitProps) {
+    return (
+        <div className="flex gap-16 mt-10">
+          {/* 특징 */}
+          <div>
+            <h3 className="text-xl font-bold">특징</h3>
+            <ul className="mt-4 space-y-2">
+                {features.map((feature, idx) => (
+                    <li key={idx} className="text-base text-[#707070]">
+                        <CheckCircle />
+                        {feature}
+                    </li>
+                ))}
+            </ul>
+          </div>
+    
+          {/* 효능 */}
+          <div>
+            <h3 className="text-xl font-bold">다양한 효능</h3>
+            <ul className="mt-4 space-y-2">
+              {benefits.map((benefit, idx) => (
+                <li key={idx} className="text-base text-[#707070]">
+                    <CheckCircle />
+                    {benefit}
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+    );
+}
