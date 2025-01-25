217fa7be28cc4929d123712e977838462396173f
jjjuni <0210leejun@gmail.com>
2025-01-25 17:46:28 +0900

    ✨ 댓글 창 페이지네이션 추가

--- src/pages/community/post/components/CommentList.tsx
+++ src/pages/community/post/components/CommentList.tsx
@@ -2,12 +2,22 @@ import React from "react";
 import comments from "../../../../mocks/comments";
 import CommentItem from "./CommentItem";
 import ReplyItem from "./ReplyItem";
+import { ArrowRightIcon } from "@public/svgs";
 
 const CommentList = () => {
+  const totalPage=2;
+  const isFirst=true;
+  const isLast=false;
+  const currentPage=1;
+
   const findParentWriter = (parentId: string) => {
     const parentComment = comments.find((comment) => comment.id === parentId);
     return parentComment?.writer || "";
   };
+
+  const pageNavigate = (page: number) => {
+    console.log(page)
+  }
 
   return (
     <div className="flex flex-col bg-gray-50 p-5">
@@ -39,6 +49,26 @@ const CommentList = () => {
           );
         }
       })}
+
+      <div className={`flex gap-3 mx-auto mt-5 items-center`}>
+        {!isFirst &&
+          <ArrowRightIcon
+            onClick={() => pageNavigate(currentPage - 1)}
+            className={`rotate-180 mb-0.5 text-gray-600 px-2 w-6 cursor-pointer`} />}
+        {[...Array(totalPage)].map((_, index) => (
+          <button
+            key={index}
+            onClick={() => pageNavigate(index + 1)}
+            disabled={currentPage === index + 1}
+            className={`px-3 py-1 text-subhead1-sb ${currentPage === index + 1 ? `text-secondary-500 cursor-default` : `text-gray-300`}`}>
+            {index + 1}
+          </button>
+        ))}
+        {!isLast &&
+          <ArrowRightIcon
+            onClick={() => pageNavigate(currentPage + 1)}
+            className={`mb-0.5 text-gray-600 px-2 w-6 cursor-pointer`} />}
+      </div>
     </div>
   );
 };
