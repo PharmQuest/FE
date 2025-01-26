fd88525a300bf4c30910408805e761f59051b98f
jjjuni <0210leejun@gmail.com>
2025-01-26 18:12:10 +0900

    💄 게시글 목록 grid 수정

--- src/pages/community/components/PostItem.tsx
+++ src/pages/community/components/PostItem.tsx
@@ -37,7 +37,7 @@ const PostItem: React.FC<PostItemProps> = ({
 
   return (
     <div className="py-3 grid grid-cols-[1fr_7fr_6fr] gap-2 border-b border-solid border-gray-100">
-      
+
       <SubjectTag text={category} />
       <div
         className="truncate flex text-body1-r text-gray-500 cursor-pointer gap-2"
@@ -48,14 +48,15 @@ const PostItem: React.FC<PostItemProps> = ({
         </p>
         {isBestPost && <Tag variant="best" />}
       </div>
-      
-      
-      <div className="grid grid-cols-[7fr_7fr_4fr_4fr_5fr] text-center text-body2-r text-gray-300 w-full">
-        <p className={`w-[70px] truncate`}>{userName}</p>
-        <p>{formattedDate}</p>
-        <p>{likeCount}</p>
-        <p>{commentCount}</p>
-        <p>{scrapeCount}</p>
+
+      <div className="grid grid-flow-col gap-5 text-center items-center justify-items-center text-body2-r text-gray-300 w-full">
+        <p className={`w-[73px] truncate`}>{userName}</p>
+        <p className={`w-[73px] truncate`}>{formattedDate}</p>
+        <div className={`flex gap-5`}>
+          <p className={`w-[36px] truncate`}>{likeCount}</p>
+          <p className={`w-[36px] truncate`}>{commentCount}</p>
+          <p className={`w-[42px] truncate`}>{scrapeCount}</p>
+        </div>
       </div>
     </div>
   );
--- src/pages/community/components/PostList.tsx
+++ src/pages/community/components/PostList.tsx
@@ -49,12 +49,14 @@ const PostList: React.FC<{category?: string, isHiddenPage?: boolean }> = ({ cate
       <div className="py-3 grid grid-cols-[1fr_7fr_6fr] gap-2 justify-items-center text-subhead1-sb text-gray-500 border-b border-solid border-gray-300">
         <p className={`w-16 text-center`}>주제</p>
         <p>제목</p>
-        <div className="grid grid-cols-[7fr_7fr_4fr_4fr_5fr] text-center w-full">
-          <p>작성자</p>
-          <p>등록일</p>
-          <p>추천</p>
-          <p>댓글</p>
-          <p>스크랩</p>
+        <div className="grid grid-flow-col gap-5 justify-items-center text-center w-full">
+          <p className={`w-[73px] truncate`}>작성자</p>
+          <p className={`w-[73px] truncate`}>등록일</p>
+          <div className={`flex gap-5`}>
+            <p className={`w-[36px] truncate`}>추천</p>
+            <p className={`w-[36px] truncate`}>댓글</p>
+            <p className={`w-[42px] truncate`}>스크랩</p>
+          </div>
         </div>
       </div>
       {postList?.map((post: Post, index: number) => (
