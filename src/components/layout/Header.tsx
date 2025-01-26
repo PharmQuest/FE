ee6650a965aad05ecb36c001ef41e8b5ebc76b32
tellgeniewish <jadastras2@gmail.com>
2025-01-26 14:58:14 +0900

    recycle: 버튼 안에 div로 수정

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -60,39 +60,24 @@ const Header = () => {
             `}>
             <LogoIcon className="min-w-fit cursor-pointer" onClick={() => router.push("/")}/>
             <div className="whitespace-nowrap flex grow h-[46px] justify-between items-center">
-              <button
-                onClick={() => router.push("/medicines")}
-                className=" px-4 py-2 bg-[#c6c6c6]/0 justify-center items-center gap-2.5 flex text-[#333333] text-xl font-bold font-['Pretendard Variable'] leading-[30px]"
-              >
-                상비약 리스트
+              <button className=" px-4 py-2 bg-[#c6c6c6]/0 justify-center items-center gap-2.5 flex text-[#333333] text-xl font-bold font-['Pretendard Variable'] leading-[30px]"
+                      onClick={() => router.push("/medicines")}>상비약 리스트</button>
+              <button className=" px-4 py-2 bg-[#c6c6c6]/0 justify-center items-center gap-2.5 flex text-[#333333] text-xl font-bold font-['Pretendard Variable'] leading-[30px]"
+                      onClick={() => router.push("/map")}>약국 찾기</button>
+              <button className=" px-4 py-2 bg-[#c6c6c6]/0 justify-center items-center gap-1 flex text-[#333333] text-xl font-bold font-['Pretendard Variable'] leading-[30px]"
+                      onClick={() => router.push("/community")}>커뮤니티
+                      {/* 로그인하면 보임 */}
+                      {!isLoggedIn && (
+                        <div className="w-[18px] h-[18px] relative">
+                          <div className="w-[18px] h-[18px] left-0 top-0 absolute bg-[#ff7700] rounded-full"></div>
+                          <div className="left-[6px] top-0 absolute text-white text-xs font-semibold font-['Pretendard Variable'] leading-[18px]">
+                            1
+                          </div>
+                        </div>
+                      )}
               </button>
-              <button
-                onClick={() => router.push("/map")}
-                className=" px-4 py-2 bg-[#c6c6c6]/0 justify-center items-center gap-2.5 flex text-[#333333] text-xl font-bold font-['Pretendard Variable'] leading-[30px]"
-              >
-                약국 찾기
-              </button>
-              <button
-                onClick={() => router.push("/community")}
-                className=" px-4 py-2 bg-[#c6c6c6]/0 justify-center items-center gap-1 flex text-[#333333] text-xl font-bold font-['Pretendard Variable'] leading-[30px]"
-              >
-                커뮤니티
-                {/* 로그인하면 보임 */}
-                {!isLoggedIn && (
-                  <div className="w-[18px] h-[18px] relative">
-                    <div className="w-[18px] h-[18px] left-0 top-0 absolute bg-[#ff7700] rounded-full"></div>
-                    <div className="left-[6px] top-0 absolute text-white text-xs font-semibold font-['Pretendard Variable'] leading-[18px]">
-                      1
-                    </div>
-                  </div>
-                )}
-              </button>
-              <button
-                onClick={() => router.push("/supplements")}
-                className=" px-4 py-2 bg-[#c6c6c6]/0 justify-center items-center gap-2.5 flex text-[#333333] text-xl font-bold font-['Pretendard Variable'] leading-[30px]"
-              >
-                해외 인기 영양제
-              </button>
+              <button className=" px-4 py-2 bg-[#c6c6c6]/0 justify-center items-center gap-2.5 flex text-[#333333] text-xl font-bold font-['Pretendard Variable'] leading-[30px]"
+                      onClick={() => router.push("/supplements")}>해외 인기 영양제</button>
               {/* 로그인 상태에 따라 버튼 렌더링 */}
               {isLoggedIn ? (
                 // isLoggedIn이 true
@@ -145,45 +130,48 @@ const Header = () => {
               className="fixed top-0 left-0 w-full h-full bg-black/30 z-40"></div>
           {/* 사이드바 부분 */}
           <div ref={sidebarRef} className="fixed top-0 left-0 h-screen w-[240px] bg-white shadow-lg z-50">
-            <div className="h-[662px] p-5">
+            <div className="h-[662px]">
               {/* 사이드바 안에 내용 */}
               <div className="flex flex-col">
+                {/* 로고와 로그인 버튼 */}
                 <div className="h-[150px] border-b border-gray-100 flex flex-col justify-center items-center">
                   <LogoIcon className="min-w-fit cursor-pointer" onClick={() => { router.push("/"); setIsSidebarOpen(false); }}/>
                   <button onClick={() => { handleLoginClick(); setIsSidebarOpen(false); }}  className="mt-[25px] w-[168px] h-10 px-[38px] py-2 bg-[#ff7700] rounded-lg justify-center items-center gap-2.5 inline-flex text-white text-base font-semibold font-['Pretendard Variable'] leading-normal">로그인</button>
-                </div>              
-                <div className="h-16 px-8 py-5 border-b border-gray-100">
-                  <button onClick={() => { router.push("/medicines"); setIsSidebarOpen(false); }} className="w-full">
+                </div>
+                {/* 메뉴 시작 */}                
+                <button onClick={() => { router.push("/medicines"); setIsSidebarOpen(false); }} className="w-full">
+                  <div className="h-16 px-8 py-5 border-b border-gray-100">
                     <div className="flex justify-between items-center text-[#333333] text-base font-bold font-['Pretendard Variable'] leading-normal">
                       <span>상비약 리스트</span>
                       <span><GTIcon/></span>
                     </div>
-                  </button>
-                </div>
-                <div className="h-16 px-8 py-5 border-b border-gray-100">
-                  <button onClick={() => { router.push("/map"); setIsSidebarOpen(false); }} className="w-full">
+                  </div>
+                </button>
+                {/* test */}
+                <button onClick={() => { router.push("/map"); setIsSidebarOpen(false); }} className="w-full">
+                  <div className="h-16 px-8 py-5 border-b border-gray-100">
                     <div className="flex justify-between items-center text-[#333333] text-base font-bold font-['Pretendard Variable'] leading-normal">
                       <span>약국 찾기</span>
                       <span><GTIcon/></span>
                     </div>
-                  </button> 
-                </div> 
-                <div className="h-16 px-8 py-5 border-b border-gray-100">     
-                  <button onClick={() => { router.push("/community"); setIsSidebarOpen(false); }} className="w-full">
+                  </div>
+                </button> 
+                <button onClick={() => { router.push("/community"); setIsSidebarOpen(false); }} className="w-full">
+                  <div className="h-16 px-8 py-5 border-b border-gray-100"> 
                     <div className="flex justify-between items-center text-[#333333] text-base font-bold font-['Pretendard Variable'] leading-normal">
                       <span>커뮤니티</span>
                       <span><GTIcon/></span>
                     </div>
-                  </button>
-                </div>
-                <div className="h-16 px-8 py-5 border-b border-gray-100">     
-                  <button onClick={() => { router.push("/supplements"); setIsSidebarOpen(false); }} className="w-full">
+                  </div>
+                </button>                   
+                <button onClick={() => { router.push("/supplements"); setIsSidebarOpen(false); }} className="w-full">
+                  <div className="h-16 px-8 py-5 border-b border-gray-100"> 
                     <div className="flex justify-between items-center text-[#333333] text-base font-bold font-['Pretendard Variable'] leading-normal">
                       <span>해외 인기 영양제</span>
                       <span><GTIcon/></span>
                     </div>
-                  </button>
-                </div>
+                  </div>
+                </button>
               </div>
             </div>
           </div>
