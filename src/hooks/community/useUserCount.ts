4d6d6d7e2205982486205b202cc0e78c70493cd3
jjjuni <0210leejun@gmail.com>
2025-01-27 21:52:04 +0900

    ✨ 좋아요 및 스크랩 관리 hook 생성

---
+++ src/hooks/community/useUserCount.ts
@@ -0,0 +1,38 @@
+import axiosInstance from "@/apis/axios-instance";
+import { useQueryClient } from "@tanstack/react-query";
+import { useEffect, useState } from "react";
+
+
+
+const useUserCount = ( url: string, queryKey: [string, number], isOnInitial: boolean, initialCount: number) => {
+  const [isOn, setIsOn] = useState(isOnInitial);
+  const [onCount, setOnCount] = useState(initialCount);
+
+  const QueryClient = useQueryClient();
+
+  const handleOn = async () => {
+    try{
+      if(!isOn){
+        await axiosInstance.post(url);
+        setOnCount(onCount + 1);
+        QueryClient.invalidateQueries({ queryKey: [queryKey] })
+      } else {
+        await axiosInstance.delete(url);
+        setOnCount(onCount - 1);
+        QueryClient.invalidateQueries({ queryKey: [queryKey] })
+      }
+      setIsOn(!isOn);
+    } catch (e) {
+      console.log(e)
+    }
+  }
+  
+  useEffect(() => {
+    setIsOn(isOnInitial);
+    setOnCount(initialCount);
+  }, [isOnInitial, initialCount]);
+
+  return { isOn, onCount, handleOn };
+}
+
+export default useUserCount;
