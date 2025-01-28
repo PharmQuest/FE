1bd593bf9e756e51141430cf2d42729d29beb2d1
9aeun <rlarkdms0424@naver.com>
2025-01-29 02:24:27 +0900

    🎨 ✨ 해외영양제 상세페이지 반응형 구현

---
+++ src/pages/supplements/components/FeatureBenefit.tsx
@@ -0,0 +1,39 @@
+import CheckCircle from '@public/svgs/check-circle.svg';
+
+
+interface FeatureBenefitProps {
+    features: string[];
+    benefits: string[];
+}
+
+export default function FeatureBenefit({ features, benefits }: FeatureBenefitProps) {
+    return (
+        <div className="flex gap-16 mt-10">
+          {/* 특징 */}
+          <div>
+            <h3 className="text-xl font-bold">특징</h3>
+            <ul className="mt-4 space-y-2">
+                {features.map((feature, idx) => (
+                    <li key={idx} className="text-base text-gray-400">
+                        <CheckCircle />
+                        {feature}
+                    </li>
+                ))}
+            </ul>
+          </div>
+    
+          {/* 효능 */}
+          <div>
+            <h3 className="text-xl font-bold">다양한 효능</h3>
+            <ul className="mt-4 space-y-2">
+              {benefits.map((benefit, idx) => (
+                <li key={idx} className="text-base text-gray-400">
+                    <CheckCircle />
+                    {benefit}
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+    );
+}
