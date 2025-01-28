5a6c910b7a615f57632dba8a12fb3a1b5470b6ff
hyesngy <tell280210@gmail.com>
2025-01-29 00:55:10 +0900

    ✨ 약국 list 및 상세정보 탭 구현

--- src/pages/map/components/PharmacyList.tsx
+++ src/pages/map/components/PharmacyList.tsx
@@ -1,10 +1,31 @@
 import React from "react";
 import PharmacyItem from "./PharmacyItem";
+import { Pharmacy, PharmacyDetails } from "./MapComponent";
 
-const PharmacyList = () => {
+interface PharmacyListProps {
+  pharmacies: Pharmacy[];
+  selectedPharmacy: PharmacyDetails | null;
+  onPharmacyClick: (pharmacy: Pharmacy) => void;
+  currentPosition: { lat: number; lng: number };
+}
+
+const PharmacyList: React.FC<PharmacyListProps> = ({
+  pharmacies,
+  selectedPharmacy,
+  onPharmacyClick,
+  currentPosition,
+}) => {
   return (
-    <div className="flex flex-col">
-      <PharmacyItem />
+    <div className="flex flex-col overflow-y-scroll flex-1">
+      {pharmacies.map((pharmacy) => (
+        <PharmacyItem
+          key={pharmacy.place_id}
+          pharmacy={pharmacy}
+          isSelected={selectedPharmacy?.place_id === pharmacy.place_id}
+          onClick={() => onPharmacyClick(pharmacy)}
+          currentPosition={currentPosition}
+        />
+      ))}
     </div>
   );
 };
