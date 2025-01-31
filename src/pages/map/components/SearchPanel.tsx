d827e8165fb64cefb68c9dcc643165df9c80b638
hyesngy <tell280210@gmail.com>
2025-01-31 18:20:45 +0900

    📱 약국찾기 반응형 구현

--- src/pages/map/components/SearchPanel.tsx
+++ src/pages/map/components/SearchPanel.tsx
@@ -25,17 +25,17 @@ const SearchPanel: React.FC<SearchPanelProps> = ({
 
   return (
     <div className="w-[380px] h-screen flex flex-col shrink-0">
-      <div className="h-[120px] bg-white border-b border-gray-100 flex py-5 px-7 flex-col justify-start items-start gap-4 self-stretch">
-        <div className="w-[324px] h-[40px] flex justify-start py-2 pl-2 pr-4 items-center gap-2 self-stretch mb-4 rounded-lg border-2 border-solid border-secondary-500">
+      <div className="lg:h-[120px] max-lg:h-[45px] bg-white border-b border-gray-100 flex py-5 px-7 flex-col justify-start items-start gap-4 self-stretch">
+        <div className="max-lg:hidden w-[324px] h-[40px] flex justify-start py-2 pl-2 pr-4 items-center gap-2 self-stretch mb-4 rounded-lg border-2 border-solid border-secondary-500">
           <SearchIcon className="w-6 h-6" />
           <input
             value={searchTerm}
             onChange={(e) => setSearchTerm(e.target.value)}
             placeholder="검색어를 입력하세요."
-            className="focus:outline-none text-gray-300 text-base font-normal font-['Pretendard Variable'] leading-normal"
+            className="focus:outline-none text-gray-600 placeholder-gray-300 text-base font-normal font-['Pretendard Variable'] leading-normal"
           />
         </div>
-        <div className="font-['Pretendard Variable'] leading-normal text-subhead1-sb text-gray-400">
+        <div className="text-subhead1-sb text-gray-400">
           검색 결과 {filteredPharmacies.length}건
         </div>
       </div>
