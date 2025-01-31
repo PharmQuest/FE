ae0669e4acb2fe2194d29ab71807d61e70b97dec
9aeun <rlarkdms0424@naver.com>
2025-01-31 10:53:11 +0900

    🎨 해외 인기 영양제 모바일 뒤로가기 버튼 추가

--- src/pages/supplements/[supplementId]/index.tsx
+++ src/pages/supplements/[supplementId]/index.tsx
@@ -12,11 +12,12 @@ import {
   warnings,
   supplements,
 } from "@/mocks/supplements";
-import { ExternalIcon } from "@public/svgs";
+import { ExternalIcon, LeftArrow } from "@public/svgs";
+import Link from "next/link";
 
 const SupplementInfo: React.FC = () => {
   const copyToClipboard = () => {
-    const url = window.location.href; // 현재 페이지 URL
+    const url = window.location.href;
     navigator.clipboard.writeText(url).then(() => {
       alert("URL이 복사되었습니다!");
     });
@@ -25,18 +26,24 @@ const SupplementInfo: React.FC = () => {
   return (
     <div className="relative w-full h-full bg-white px-4 py-10">
       <div className="max-w-screen-lg mx-auto flex flex-col gap-10">
-        {/* 제품 기본 정보 */}
         <div className="relative">
-          <h2 className="text-2xl font-bold text-gray-600 mb-4">제품 기본 정보</h2>
+          <div className="w-full max-w-[1000px] px-4 md:px-0 py-4 flex justify-between items-center md:shadow-none shadow-[0px_2px_0px_0px_rgba(0,0,0,0.05)] mb-4">
+            <div className="flex items-center gap-2">
+              <Link href="/mypage">
+                <LeftArrow className="w-6 h-6 text-gray-600 sm:block lg:hidden" />
+              </Link>
+              <h2 className="text-2xl font-bold text-gray-600">제품 기본 정보</h2>
+            </div>
 
-          {/* URL 복사 버튼 */}
-          <button
-            onClick={copyToClipboard}
-            className="absolute top-0 right-0 z-10 flex items-center gap-1 text-gray-500 text-sm hover:text-gray-700"
-          >
-            <ExternalIcon />        
-            <span>URL 복사</span>
-          </button>
+            {/* URL 복사 버튼 */}
+            <button
+              onClick={copyToClipboard}
+              className="flex items-center gap-1 text-gray-500 text-sm hover:text-gray-700"
+            >
+              <ExternalIcon />
+              <span>URL 복사</span>
+            </button>
+          </div>
 
           {/* 제품 정보 컴포넌트 */}
           <ProductBasicInfo
@@ -67,7 +74,7 @@ const SupplementInfo: React.FC = () => {
 
         {/* 영양제 더보기 */}
         <div>
-          <MoreSupplements supplements={supplements} imageWidth={287}/>
+          <MoreSupplements supplements={supplements} imageWidth={287} />
         </div>
       </div>
     </div>
