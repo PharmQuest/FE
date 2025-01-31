24b64e9e08134fa46953514e371d7f4762218bc2
hyesngy <tell280210@gmail.com>
2025-01-31 14:49:17 +0900

    ✨ 소셜 로그인 webp 추가

--- public/images/index.ts
+++ public/images/index.ts
@@ -1,1 +1,4 @@
 export { default as MedicineImage } from "./medicine.webp";
+export { default as NaverLoginButton } from "./naver-login.webp";
+export { default as KakaoLoginButton } from "./kakao-login.webp";
+export { default as GoogleLoginButton } from "./google-login.webp";
