3a99f22a43096517b78b2169d4931ef7fe2647dc
hyesngy <tell280210@gmail.com>
2025-02-01 22:08:50 +0900

    🚚 GrayButton TextButton으로 변경 및 스타일추가

--- src/pages/community/post/components/CommentInput.tsx
+++ src/pages/community/post/components/CommentInput.tsx
@@ -1,11 +1,16 @@
 import React, { useState } from "react";
-import GrayButton from "../../components/GrayButton";
+import TextButton from "../../components/TextButton";
 import axiosInstance from "@/apis/axios-instance";
 import { useParams } from "next/navigation";
 import { useQueryClient } from "@tanstack/react-query";
 
-const CommentInput = ({ replyParentId, userName }: { replyParentId?: number, userName?: string }) => {
-
+const CommentInput = ({
+  replyParentId,
+  userName,
+}: {
+  replyParentId?: number;
+  userName?: string;
+}) => {
   const params = useParams() || {};
   const postId = params.postId || null;
 
@@ -13,26 +18,31 @@ const CommentInput = ({ replyParentId, userName }: { replyParentId?: number, use
 
   const handleClick = async () => {
     try {
-      await axiosInstance.post(`${process.env.NEXT_PUBLIC_DOMAIN}/community/posts/${postId}/comments`,
+      await axiosInstance.post(
+        `${process.env.NEXT_PUBLIC_DOMAIN}/community/posts/${postId}/comments`,
         { content },
         {
           params: {
             parentsId: replyParentId,
           },
-        })
+        }
+      );
       setContent("");
-      queryClient.invalidateQueries({ queryKey: ["post", Number(postId)] })
+      queryClient.invalidateQueries({ queryKey: ["post", Number(postId)] });
     } catch (error) {
       console.log(error);
     }
   };
 
-  const [content, setContent] = useState<string>(userName ? `@${userName} ` : "");
-
-
+  const [content, setContent] = useState<string>(
+    userName ? `@${userName} ` : ""
+  );
 
   return (
-    <form action={handleClick} className="flex flex-col gap-2 w-full pt-2 pb-3 pr-3 pl-4 rounded border border-solid border-gray-200 bg-white">
+    <form
+      action={handleClick}
+      className="flex flex-col gap-2 w-full pt-2 pb-3 pr-3 pl-4 rounded border border-solid border-gray-200 bg-white"
+    >
       <p className="text-subhead1-sb text-gray-600">닉네임닉네임</p>
       <input
         placeholder="댓글을 남겨보세요."
@@ -43,10 +53,11 @@ const CommentInput = ({ replyParentId, userName }: { replyParentId?: number, use
       />
       <div className="flex flex-row gap-3 justify-end">
         <span className="text-body1-r text-gray-300">{content.length}/600</span>
-        <GrayButton
+        <TextButton
           text="등록"
           onClick={handleClick}
-          color={content.length > 0 ? "green" : "gray"} />
+          color={content.length > 0 ? "green" : "gray"}
+        />
       </div>
     </form>
   );
