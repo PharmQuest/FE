d6daa85d07134c271ea0a3d234e58da9cd4c61e6
jjjuni <0210leejun@gmail.com>
2025-02-01 21:23:10 +0900

    💄 커뮤니티 메인 페이지 반응형 구현

--- src/pages/community/index.tsx
+++ src/pages/community/index.tsx
@@ -43,42 +43,81 @@ export default function Community() {
 
   return (
     <div>
-      <div className="max-w-[900px] mx-auto mt-9">
-        <div className="flex flex-row gap-x-4 mt-3">
-          <div className={`flex flex-col`}>
+      <div
+        className="
+          lg:max-w-[900px] lg:mx-auto lg:px-0
+          md:max-w-[600px] md:mx-auto md:px-0 md:mt-9
+          w-full px-5">
+        <div
+          className="
+            lg:w-fit lg:flex lg:flex-row lg:gap-x-4
+            mt-3 grow">
+          <div className={`flex flex-col grow`}>
             <div className={`flex justify-between mb-3`}>
-              <p className="text-display2-b text-gray-600">BEST 인기글</p>
               <p
-                className={`flex text-gray-400 text-subhead1-sb items-center gap-2 cursor-pointer mr-2`}
+                className="
+                  lg:text-display2-b
+                  flex text-m-headline1-b text-gray-600 items-center gap-2">
+                BEST 인기글
+                <ArrowRightIcon
+                  className={`
+                    lg:hidden
+                    content-center mb-0.5 h-3`}
+                  onClick={() => router.push('/community/bestposts')} />
+              </p>
+              <p
+                className={`
+                  lg:flex
+                  hidden text-gray-400 text-subhead1-sb items-center gap-2 cursor-pointer mr-2`}
                 onClick={() => router.push('/community/bestposts')}>
                 더보기
-                <ArrowRightIcon className={`content-center mb-0.5`} />
+                <ArrowRightIcon className={`content-center mb-0.5 h-2.5`} />
               </p>
             </div>
-            <PopularPostList posts={popularPosts.slice(0, 3)} bgColor={"primary-50"} gap={3}/>
+            <PopularPostList posts={popularPosts.slice(0, 3)} bgColor={"primary-50"} gap={true} />
           </div>
-          <div>
+          <div className={`lg:block hidden`}>
             <p className={`h-9 mb-3`} />
             <UserNavbar />
           </div>
         </div>
-        <div className="flex flex-row items-center gap-3 mt-20 mb-4">
-          <p className="text-display2-b text-gray-600">게시글</p>
-          {categories.map((category) => (
-            <FilterButton
-              key={category.text}
-              text={category.text}
-              isSelected={category.isSelected}
-              onClickFn={() => handleFilterButton(category)} />
-          ))}
+        <div className="lg:mt-20 lg:mb-4 flex flex-row items-center gap-3 mt-9">
           <p
-            className={`flex text-gray-400 text-subhead1-sb items-center gap-2 cursor-pointer mr-4 ml-auto mt-3`}
+            className="
+              lg:text-display2-b 
+              flex text-m-headline1-b text-gray-600 items-center gap-2">
+            게시글
+            <ArrowRightIcon
+              className={`
+                lg:hidden
+                content-center mb-0.5 h-3`}
+              onClick={() => router.push('/community/posts')} />
+          </p>
+
+          <div
+            className={`
+              lg:flex 
+              gap-3 hidden`}>
+            {categories.map((category) => (
+              <FilterButton
+                key={category.text}
+                text={category.text}
+                isSelected={category.isSelected}
+                onClickFn={() => handleFilterButton(category)} />
+            ))}
+          </div>
+          <p
+            className={`
+              lg:flex
+              hidden text-gray-400 text-subhead1-sb items-center gap-2 cursor-pointer mr-4 ml-auto mt-3`}
             onClick={() => router.push('/community/posts')}>
             더보기
-            <ArrowRightIcon className={`content-center mb-0.5`} />
+            <ArrowRightIcon className={`content-center mb-0.5 h-2.5`} />
           </p>
         </div>
-        <PostList page={1} category={category} isPageHidden={true} postLimit={10} />
+        <div>
+          <PostList page={1} category={category} isPageHidden={true} postLimit={10} />
+        </div>
       </div>
     </div>
   );
