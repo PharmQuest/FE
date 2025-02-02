a662b8c7d00712777fa78f4e5adb0940c440eba8
jjjuni <0210leejun@gmail.com>
2025-02-03 08:53:16 +0900

    ✨ 댓글 메뉴 라벨 추가

---
+++ src/pages/community/post/components/CommentMenu.tsx
@@ -0,0 +1,60 @@
+import useStore from "@/store/useStore";
+import { useRouter } from "next/router";
+import { MouseEvent } from "react";
+
+interface CommentMenuProp {
+  commentId: number;
+  isMenuOpen: boolean;
+  setIsMenuOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  isOwnComment: boolean;
+}
+
+const CommentMenu: React.FC<CommentMenuProp> = ({ commentId, isMenuOpen, setIsMenuOpen, isOwnComment }) => {
+
+  const router = useRouter();
+
+  const {
+    setNoticeModalText,
+    setIsNoticeModalOpen,
+    setIsReportModalOpen,
+  } = useStore();
+
+  const handleModify = () => {
+    
+  }
+
+  const handleDelete = async () => {
+    
+  }
+
+  const handleReport = (e: MouseEvent) => {
+    e.stopPropagation();
+    setIsReportModalOpen(true)
+    setIsMenuOpen(false)
+  }
+
+  return (
+    isMenuOpen && (
+      isOwnComment ? (
+        <div className={`absolute right-0 top-[30px] w-[100px] px-2 shadow-custom-light bg-white text-gray-600 text-subhead1-sb rounded`}>
+          <div
+            className={`px-1 py-3 border-b border-solid border-gray-100`}
+            onClick={() => handleModify()}>수정하기
+          </div>
+          <div
+            className={`px-1 py-3`}
+            onClick={() => handleDelete()}>삭제하기
+          </div>
+        </div>
+      ) : (
+        <div className={`absolute right-0 top-[30px] w-[96px] px-2 shadow-custom-light bg-white text-gray-600 text-subhead1-sb rounded`}>
+          <div
+            className={`px-1 py-3`}
+            onClick={(e) => handleReport(e)}>신고하기</div>
+        </div>
+      )
+    )
+  )
+}
+
+export default CommentMenu;
