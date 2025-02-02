cc98e0067055785624593f0e459365c7f9ee09a2
9aeun <rlarkdms0424@naver.com>
2025-02-02 15:00:43 +0900

    ✨ 로그인 api 연동 완료

--- src/apis/auth.ts
+++ src/apis/auth.ts
@@ -1,8 +1,12 @@
-const BASE_URL = "http://ec2-54-180-74-54.ap-northeast-2.compute.amazonaws.com:8080";
+const BASE_URL = `http://ec2-54-180-74-54.ap-northeast-2.compute.amazonaws.com:8080
+`;
 
 export const login = async (provider: "kakao" | "naver" | "google") => {
+  if (!BASE_URL) {
+    console.error("🚨 로그인 URL이 설정되지 않았습니다.");
+    return;
+  }
   try {
-    // OAuth 인증 페이지로 이동
     window.location.href = `${BASE_URL}/oauth2/authorization/${provider}`;
   } catch (error) {
     console.error("로그인 요청 실패:", error);
