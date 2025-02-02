bbc37af55bb9c2ce7a9a37b4b83aa1a3b8f6853c
jjjuni <0210leejun@gmail.com>
2025-02-03 06:35:58 +0900

    ♻️ axiosInstance 추가 및 수정

--- src/apis/axios-instance.ts
+++ src/apis/axios-instance.ts
@@ -1,8 +1,15 @@
 import axios from "axios";
 
-const axiosInstance = axios.create({
+export const axiosInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
 });
+
+export const axiosPostInstance = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
+  headers: {
+    "Content-Type": "multipart/form-data",
+  }
+})
 
 axiosInstance.interceptors.request.use((config) => {
   if (typeof window !== "undefined") {
@@ -14,4 +21,12 @@ axiosInstance.interceptors.request.use((config) => {
   return config;
 });
 
-export default axiosInstance;
+axiosPostInstance.interceptors.request.use((config) => {
+  if (typeof window !== "undefined") {
+    const token = localStorage.getItem("accessToken");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+  }
+  return config;
+});
