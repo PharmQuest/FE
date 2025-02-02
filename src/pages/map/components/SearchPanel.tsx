ec6328b41bae2c5a09f7918c7852bbc79a3a701c
hyesngy <tell280210@gmail.com>
2025-02-02 10:02:24 +0900

    🐛 검색패널 하단 잘리는 문제해결

--- src/pages/map/components/SearchPanel.tsx
+++ src/pages/map/components/SearchPanel.tsx
@@ -24,20 +24,20 @@ const SearchPanel: React.FC<SearchPanelProps> = ({
     ) ?? [];
 
   return (
-    <div className="w-[380px] max-sm:w-screen h-screen flex flex-col shrink-0">
-      <div className="lg:h-[120px] max-lg:h-[45px] bg-white border-b border-gray-100 flex py-5 px-7 flex-col justify-start items-start gap-4 self-stretch">
-        <div className="max-lg:hidden w-[324px] h-[40px] flex justify-start py-2 pl-2 pr-4 items-center gap-2 self-stretch mb-4 rounded-lg border-2 border-solid border-secondary-500">
+    <div className="w-[380px] max-sm:w-screen h-full flex flex-col">
+      <div className="lg:h-[132px] h-[45px] bg-white border-b border-gray-100 flex lg:pt-8 max-lg:justify-center px-7 flex-col items-start gap-4 self-stretch">
+        <div className="max-lg:hidden w-[324px] h-[40px] flex justify-start py-2 pl-2 pr-4 items-center gap-2 self-stretch rounded-lg border-2 border-solid border-secondary-500">
           <SearchIcon className="w-6 h-6" />
           <input
             value={searchTerm}
             onChange={(e) => setSearchTerm(e.target.value)}
             placeholder="검색어를 입력하세요."
-            className="focus:outline-none text-gray-600 placeholder-gray-300 text-base font-normal font-['Pretendard Variable'] leading-normal"
+            className="focus:outline-none text-gray-600 placeholder-gray-300 text-body1-r"
           />
         </div>
-        <div className="text-subhead1-sb text-gray-400">
+        <p className="lg:text-subhead1-sb text-m-subhead1-sb text-gray-400">
           검색 결과 {filteredPharmacies.length}건
-        </div>
+        </p>
       </div>
       <PharmacyList
         pharmacies={filteredPharmacies}
