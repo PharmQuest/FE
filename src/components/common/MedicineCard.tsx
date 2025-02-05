4e445cd512e8c858cade91bee1cb28eb2dc14b55
hyesngy <tell280210@gmail.com>
2025-02-05 15:21:17 +0900

    ♻️ MedicineCard props 추가

--- src/components/common/MedicineCard.tsx
+++ src/components/common/MedicineCard.tsx
@@ -3,88 +3,112 @@ import Image from "next/image";
 import { useRouter } from "next/router";
 import { useState } from "react";
 
-const MedicineCard = () => {
+interface MedicineCardProps {
+  brandName: string;
+  genericName: string;
+  splSetId: string;
+  imgUrl: string;
+  category: string;
+  country: string;
+}
 
+const MedicineCard: React.FC<MedicineCardProps> = ({
+  brandName,
+  genericName,
+  splSetId,
+  imgUrl,
+  category,
+  country,
+}) => {
   const router = useRouter();
-
   const [isBookmark, setIsBookmark] = useState(false);
-
-  const [src, setSrc] = useState("/url");
+  const [src, setSrc] = useState(imgUrl);
 
   const handleBoomark = (e: React.MouseEvent) => {
-    e.stopPropagation();    // 부모 컴포넌트 클릭 방지
+    e.stopPropagation(); // 부모 컴포넌트 클릭 방지
     setIsBookmark(!isBookmark);
     // 추후에 추가할 북마크 추가 기능 API ~~
-  }
+  };
 
   return (
-    <div 
+    <div
       className={`
         md:p-5 md:pr-4 md:rounded-lg md:h-[178px]
-        h-[124px] border-gray-100 border p-3 flex items-center hover:cursor-pointer rounded truncate`} 
-      onClick={() => router.push('medicines/1')}>
+        h-[124px] border-gray-100 border p-3 flex items-center hover:cursor-pointer rounded truncate`}
+      onClick={() => router.push(`medicines/${splSetId}`)}
+    >
       {/* medicine-image */}
-      <div className={`
+      <div
+        className={`
         md:w-[138px] md:h-[138px] 
-        rounded w-[100px] h-[100px] flex items-center`}>
-        <Image 
+        rounded w-[100px] h-[100px] flex items-center`}
+      >
+        <Image
           src={src}
-          alt="이미지"
+          alt={`${brandName} 이미지`}
           width="100"
           height="100"
           className={`w-full rounded`}
           onError={() => setSrc("/images/no_image.webp")}
-          priority/>
+          priority
+        />
       </div>
       {/* info-wrapper */}
-      <div 
+      <div
         className={`
           md:gap-3 md:ml-4 
-          flex flex-col gap-2 ml-3 grow`}>
+          flex flex-col gap-2 ml-3 grow`}
+      >
         {/* tag */}
-        <div 
+        <div
           className={`
             md:text-body2-r text-white bg-primary-200 w-fit rounded px-2 pt-0.5 pb-[1px] text-center
             text-m-caption2-r
-            `}>
-          미국
+            `}
+        >
+          {country}
         </div>
-        <div 
+        <div
           className={`
             md:gap-6
-            flex gap-2`}>
-          <div 
+            flex gap-2`}
+        >
+          <div
             className={`
               md:gap-1 md:w-14 md:text-subhead1-sb
               flex flex-col text-m-subhead1-sb gap-0.5
-              `}>
+              `}
+          >
             <p>제품명</p>
             <p>일반명</p>
             <p>분류</p>
           </div>
-          <div 
+          <div
             className={`
               md:gap-1 md:text-body1-r
-              flex flex-col text-m-body2-r gap-0.5`}>
-            <p className={`max-w-[100px] truncate`}>TYLENOL</p>
-            <p className={`max-w-[100px] truncate`}>아세트아미노펜</p>
-            <p className={`max-w-[100px] truncate`}>진통/해열</p>
+              flex flex-col text-m-body2-r gap-0.5`}
+          >
+            <p className={`max-w-[100px] truncate`}>{brandName}</p>
+            <p className={`max-w-[100px] truncate`}>{genericName}</p>
+            <p className={`max-w-[100px] truncate`}>{category}</p>
           </div>
         </div>
       </div>
-      <div 
+      <div
         className={`
-          h-[100%] relative min-w-[30px]`}>
-        <BookmarkIcon 
+          h-[100%] relative min-w-[30px]`}
+      >
+        <BookmarkIcon
           className={`
             md:w-[30px]
-            absolute top-0 right-0 w-6`} 
-          onClick={(e: React.MouseEvent) => handleBoomark(e)} 
-          stroke={isBookmark ? '#FFD755' : '#707070'}
-          fill={isBookmark ? '#FFD755' : 'none'} />
+            absolute top-0 right-0 w-6`}
+          onClick={(e: React.MouseEvent) => handleBoomark(e)}
+          stroke={isBookmark ? "#FFD755" : "#707070"}
+          fill={isBookmark ? "#FFD755" : "none"}
+        />
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default MedicineCard;
