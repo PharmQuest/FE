5617e481e787745e8cf3da8a6fa140e4c3a8516a
hyesngy <tell280210@gmail.com>
2025-02-05 15:45:44 +0900

    🔧 nextConfig에 images 외부 도메인 추가

--- next.config.ts
+++ next.config.ts
@@ -1,32 +1,35 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-	/* config options here */
-	reactStrictMode: true,
-	
-	images: {
-		domains: [process.env.NEXT_PUBLIC_IMAGE_DOMAIN || ''], // 여기에 외부 도메인 추가
-	},
+  /* config options here */
+  reactStrictMode: true,
 
-	webpack(config) {
-		config.module.rules.push({
-			test: /\.svg$/i,
-			use: ["@svgr/webpack"],
-		});
+  images: {
+    domains: [
+      "dailymed.nlm.nih.gov",
+      process.env.NEXT_PUBLIC_IMAGE_DOMAIN || "",
+    ],
+  },
 
-		return config;
-	},
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      use: ["@svgr/webpack"],
+    });
 
-	experimental: {
-		turbo: {
-			rules: {
-				"*.svg": {
-					loaders: ["@svgr/webpack"],
-					as: "*.js",
-				},
-			},
-		},
-	},
+    return config;
+  },
+
+  experimental: {
+    turbo: {
+      rules: {
+        "*.svg": {
+          loaders: ["@svgr/webpack"],
+          as: "*.js",
+        },
+      },
+    },
+  },
 };
 
 export default nextConfig;
