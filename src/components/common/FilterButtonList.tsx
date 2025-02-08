bd915e3f773ccc712128252c08c55516dc7847a4
tellgeniewish <jadastras2@gmail.com>
2025-02-08 11:06:29 +0900

    :sparkles: 필터버튼 클릭 시 주소로 이동

--- src/components/common/FilterButtonList.tsx
+++ src/components/common/FilterButtonList.tsx
@@ -3,7 +3,7 @@ import FilterButton from "./FilterButton";
 interface filterInfo {
   text: string;
   isSelected?: boolean;
-  onClckFn?: () => void;
+  onClickFn?: () => void;
   isHomeButton?: boolean;
 }
 
@@ -12,7 +12,7 @@ const FilterButtonList = ({ filterLists, className }: { filterLists: filterInfo[
   return (
     <div className={`flex gap-2 overflow-x-auto scrollbar-hide ${className}`}>
       {filterLists.map((item, index) => (
-        <FilterButton key={index} text={item.text} isHomeButton={item.isHomeButton} />
+        <FilterButton key={index} text={item.text} isHomeButton={item.isHomeButton} onClickFn={item.onClickFn} />
       ))}
     </div>
   )
