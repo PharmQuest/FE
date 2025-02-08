31fc2705d6eb500ddea951287975d7abfc286dd2
hyesngy <tell280210@gmail.com>
2025-02-09 04:56:43 +0900

    ✨ 상비약 리스트 페이지네이션 및 카테고리 연동

--- src/pages/medicines/index.tsx
+++ src/pages/medicines/index.tsx
@@ -1,8 +1,9 @@
-// 상비약 리스트
 import FilterButton from "@/components/common/FilterButton";
 import MedicineCardList from "@/components/common/MedicineCardList";
-import { useMedicineList } from "@/hooks/community/useMedicineList";
+import { useMedicineList } from "@/hooks/medicine/useMedicineList";
+import useFormatCategory from "@/hooks/medicine/useFormatMedicineCategory";
 import { useState } from "react";
+import PageNavigator from "../community/components/PageNavigator";
 
 const FILTER_CATEGORY = [
   "전체",
@@ -18,13 +19,12 @@ const FILTER_CATEGORY = [
 
 const Medicine = () => {
   const [selectedCategory, setSelectedCategory] = useState<string>("전체");
+  const [page, setPage] = useState(1);
+  const formatCategory = useFormatCategory();
 
-  const {
-    data: medicines,
-    isLoading,
-    isError,
-  } = useMedicineList({
-    category: selectedCategory,
+  const { data, isLoading, isError } = useMedicineList({
+    category: formatCategory(selectedCategory),
+    page,
   });
 
   if (isLoading) {
@@ -43,34 +43,42 @@ const Medicine = () => {
     );
   }
 
+  const handlePageChange = (newPage: number) => {
+    setPage(newPage);
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+
   return (
     <>
-      {/* container */}
-      <div
-        className={`
-        lg:w-[900px] lg:mx-auto
-        md:w-[600px] md:mx-auto
-        sm:w-full 
-        min-h-[calc(100vh-412px)] w-full px-5`}
-      >
-        <div
-          className={`
-            md:text-display2-b md:my-9
-            text-m-subhead1-sb flex items-center gap-3 w-100% my-3`}
-        >
-          <h1 className={`min-w-fit`}>전체</h1>
+      <div className="lg:w-[900px] lg:mx-auto md:w-[600px] md:mx-auto sm:w-full min-h-[calc(100vh-412px)] w-full px-5">
+        <div className="md:text-display2-b md:my-9 text-m-subhead1-sb flex items-center gap-3 w-100% my-3">
+          <h1 className="min-w-fit">{selectedCategory}</h1>
 
-          <div
-            className={`
-              md:flex
-              hidden h-fit content-center gap-2 overflow-y-scroll scrollbar-hide`}
-          >
+          <div className="md:flex hidden h-fit content-center gap-2 overflow-y-scroll scrollbar-hide">
             {FILTER_CATEGORY?.map((item, index) => (
-              <FilterButton key={index} text={item} />
+              <FilterButton
+                key={index}
+                text={item}
+                isSelected={item === selectedCategory}
+                onClickFn={() => {
+                  setSelectedCategory(item);
+                  setPage(1);
+                }}
+              />
             ))}
           </div>
         </div>
-        <MedicineCardList medicines={medicines || []} />
+        <MedicineCardList medicines={data?.medicines || []} />
+        {data?.pagination && (
+          <PageNavigator
+            totalPage={data.pagination.totalPages}
+            isFirst={page === 1}
+            isLast={page === data.pagination.totalPages}
+            page={page}
+            setPage={setPage}
+            className="mt-8"
+          />
+        )}
       </div>
     </>
   );
