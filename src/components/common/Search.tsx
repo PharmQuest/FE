61e02d17314a1b1d20a79bd14e82914957c2c1ec
jjjuni <0210leejun@gmail.com>
2025-02-09 22:15:34 +0900

    ✨ 검색창 컴포넌트 및 필터 버튼 컴포넌트 라우팅

--- src/components/common/Search.tsx
+++ src/components/common/Search.tsx
@@ -9,26 +9,59 @@ const Search = () => {
 
   const router = useRouter();
   const pathName = router.pathname;
+  const segments = pathName.split("/");
   const isHomePage = pathName === '/' ? true : false;
 
   const [searchText, setSearchText] = useState("");
   const [countryValue, setCountryValue] = useState("");
   const [countryText, setCountryText] = useState("전체");
 
+  const [searchUrl, setSearchUrl] = useState("");
+
   const [isSearchModalOpen, setIsSearchModalOpen] = useState(false);
 
-  const handleSearchModal = (e:MouseEvent) => {
+  const handleSearchModal = (e: MouseEvent) => {
     e.stopPropagation();
     setIsSearchModalOpen(!isSearchModalOpen);
   }
 
+  const handleSearch = () => {
+    if (searchText.trim() !== "") {
+      router.push(
+        {
+          pathname: searchUrl,
+          query: {...router.query, "keyword": searchText, "country": countryValue || "NONE"},
+        }
+      )
+    }
+  }
+
+  useEffect(() => {
+    switch (segments[1]) {
+      case "community":
+        setSearchUrl("/community/posts/search")
+        break;
+
+      case "supplements":
+        setSearchUrl("/supplements")
+        break;
+
+      default:
+        setSearchUrl("/medicines")
+        break;
+    }
+  }, [segments])
+
+  useEffect(() => {
+    setIsSearchModalOpen(false);
+  }, [router.pathname])
+
   useEffect(() => {
     setCountryValue("");
     setCountryText("전체");
+    setSearchText("")
     setIsSearchModalOpen(false);
-  }, [router])
-
-
+  }, [segments[1]])
 
   return (
     <div className="w-full flex items-center gap-4">
@@ -37,7 +70,7 @@ const Search = () => {
           ${isHomePage ? `lg:px-6 lg:w-[900px] lg:gap-2 md:w-[601px] w-full lg:h-[54px] h-[37px] px-4 py-2 gap-2.5` : `lg:px-[16px] h-10 px-4 py-2 gap-4`} 
           relative flex items-center bg-white rounded-full flex-grow w-full`}>
         {/* 위치선택 */}
-        <button 
+        <button
           className={`flex items-center lg:gap-2 gap-1 bg-none rounded-full text-gray-500 outline-none`}
           onClick={(e) => handleSearchModal(e)}>
           {/* 반응형 아이콘 */}
@@ -57,7 +90,9 @@ const Search = () => {
           `}>{countryText}</p>
         </button>
         <SearchBarIcon className={`${isHomePage ? `lg:ml-3 lg:mr-2 lg:h-[22px] h-3` : `h-4`}`} />
-        <div className={`lg:gap-2 flex gap-1.5 grow`}>
+        <form
+          action={handleSearch}
+          className={`lg:gap-2 flex gap-1.5 grow`}>
           <SearchIcon className={`${isHomePage ? `lg:w-[30px] w-5 lg:ml-[2px]` : `w-6`} ml-[-2px]`} />
           <input
             type="text"
@@ -66,14 +101,14 @@ const Search = () => {
             value={searchText}
             onChange={(e) => setSearchText(e.target.value)}
           />
-        </div>
+        </form>
         {searchText &&
           <XIcon
             className={`cursor-pointer ${isHomePage ? `lg:w-5 w-3` : `lg:w-4 w-[15px] mr-1`}`}
             onClick={() => setSearchText('')} />
         }
 
-        <SearchModal countryValue={countryValue} setCountryValue={setCountryValue} setCountryText={setCountryText} isSearchModalOpen={isSearchModalOpen} setIsSearchModalOpen={setIsSearchModalOpen}/>
+        <SearchModal countryValue={countryValue} setCountryValue={setCountryValue} setCountryText={setCountryText} isSearchModalOpen={isSearchModalOpen} setIsSearchModalOpen={setIsSearchModalOpen} />
       </div>
     </div>
   );
--- src/constants/FilterList.ts
+++ src/constants/FilterList.ts
@@ -1,26 +1,187 @@
+/* eslint-disable */
+
+const Community = `/community/posts?category=`
+const Medicine = `/medicines?category=`
+const supplement = `/supplements?category=`
+
 export const HOME_FILTER_LIST = [
   {
     text: "진통/해열",
+    isHomeButton: true,
   },
   {
     text: "소화/위장",
+    isHomeButton: true,
   },
   {
     text: "감기/기침",
+    isHomeButton: true,
   },
   {
     text: "알레르기",
+    isHomeButton: true,
   },
   {
     text: "상처/소독",
+    isHomeButton: true,
   },
   {
     text: "멀미",
+    isHomeButton: true,
   },
   {
     text: "안약",
+    isHomeButton: true,
   },
   {
     text: "기타",
+    isHomeButton: true,
+  },
+]
+
+export const MOBILE_COMMUNIY_FILTER_LIST = [
+  {
+    text: "전체",
+    isMobileButton: true,
+    url: `${Community}ALL`,
+    value: 'ALL',
+  },
+  {
+    text: "자유",
+    isMobileButton: true,
+    url: `${Community}FORUM`,
+    value: 'FORUM',
+  },
+  {
+    text: "약국",
+    isMobileButton: true,
+    url: `${Community}PHARMACY`,
+    value: 'PHARMACY',
+  },
+  {
+    text: "병원",
+    isMobileButton: true,
+    url: `${Community}HOSPITAL`,
+    value: 'HOSPITAL',
+  },
+  {
+    text: "약",
+    isMobileButton: true,
+    url: `${Community}MEDICATION`,
+    value: 'MEDICATION',
+  },
+  {
+    text: "증상",
+    isMobileButton: true,
+    url: `${Community}SYMPTOM`,
+    value: 'SYMPTOM',
+  },
+  {
+    text: "영양제",
+    isMobileButton: true,
+    url: `${Community}SUPPLEMENT`,
+    value: 'SUPPLEMENT',
+  },
+]
+
+export const MOBILE_MEDICINE_FILTER_LIST = [
+  {
+    text: "전체",
+    isMobileButton: true,
+    url: `${Medicine}ALL`,
+    value: `ALL`,
+  },
+  {
+    text: "진통/해열",
+    isMobileButton: true,
+    url: `${Medicine}PAIN_RELIEF`,
+    value: `PAIN_RELIEF`,
+  },
+  {
+    text: "소화/위장",
+    isMobileButton: true,
+    url: `${Medicine}DIGESTIVE`,
+    value: `DIGESTIVE`,
+  },
+  {
+    text: "감기/기침",
+    isMobileButton: true,
+    url: `${Medicine}COLD`,
+    value: `COLD`,
+  },
+  {
+    text: "알레르기",
+    isMobileButton: true,
+    url: `${Medicine}ALLERGY`,
+    value: `ALLERGY`,
+  },
+  {
+    text: "상처/소독",
+    isMobileButton: true,
+    url: `${Medicine}ANTISEPTIC`,
+    value: `ANTISEPTIC`,
+  },
+  {
+    text: "멀미",
+    isMobileButton: true,
+    url: `${Medicine}MOTION_SICKNESS`,
+    value: `MOTION_SICKNESS`,
+  },
+  {
+    text: "안약",
+    isMobileButton: true,
+    url: `${Medicine}EYE_DROPS`,
+    value: `EYE_DROPS`,
+  },
+  {
+    text: "기타",
+    isMobileButton: true,
+    url: `${Medicine}OTHER`,
+    value: `OTHER`,
+  },
+]
+
+export const MOBILE_SUPPLEMENT_FILTER_LIST = [
+  {
+    text: "전체",
+    isMobileButton: true,
+    url: `${supplement}전체`,
+    value: "전체",
+  },
+  {
+    text: "면역력 강화",
+    isMobileButton: true,
+    url: `${supplement}면역`,
+    value: "면역",
+  },
+  {
+    text: "피로회복",
+    isMobileButton: true,
+    url: `${supplement}피로`,
+    value: "피로",
+  },
+  {
+    text: "소화건강",
+    isMobileButton: true,
+    url: `${supplement}소화`,
+    value: "소화",
+  },
+  {
+    text: "피부건강",
+    isMobileButton: true,
+    url: `${supplement}피부`,
+    value: "피부",
+  },
+  {
+    text: "뼈관절건강",
+    isMobileButton: true,
+    url: `${supplement}관절`,
+    value: "관절",
+  },
+  {
+    text: "눈건강",
+    isMobileButton: true,
+    url: `${supplement}눈건강`,
+    value: "눈건강",
   },
 ]
