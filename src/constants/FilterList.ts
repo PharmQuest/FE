45b097dfa2477b64ffc17fbc61ead8c4f835b77c
jjjuni <0210leejun@gmail.com>
2025-02-09 17:10:25 +0900

    ✨ 필터리스트 상수 추가

---
+++ src/constants/FilterList.ts
@@ -0,0 +1,26 @@
+export const HOME_FILTER_LIST = [
+  {
+    text: "진통/해열",
+  },
+  {
+    text: "소화/위장",
+  },
+  {
+    text: "감기/기침",
+  },
+  {
+    text: "알레르기",
+  },
+  {
+    text: "상처/소독",
+  },
+  {
+    text: "멀미",
+  },
+  {
+    text: "안약",
+  },
+  {
+    text: "기타",
+  },
+]
