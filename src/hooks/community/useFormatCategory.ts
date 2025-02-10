236cbcd00b9ed4fbb44ce38f5a883ba9737e4bd5
jjjuni <0210leejun@gmail.com>
2025-02-10 20:20:39 +0900

    ✨ 나의 활동 조회 및 삭제 기능 추가

--- src/hooks/community/useFormatCategory.ts
+++ src/hooks/community/useFormatCategory.ts
@@ -3,28 +3,53 @@ const useFormatCategory = () => {
     switch (category) {
       case "자유":
         return "FORUM";
-  
+
       case "약국":
         return "PHARMACY";
-  
+
       case "병원":
         return "HOSPITAL";
-  
+
       case "약":
         return "MEDICATION";
-  
+
       case "증상":
         return "SYMPTOM";
-  
+
       case "영양제":
         return "SUPPLEMENT";
-  
+
       default:
         return "ALL";
     }
-  }
+  };
 
-  return formatCategory;
+  const unformatCategory = (category: string) => {
+    switch (category) {
+      case "FORUM":
+        return "자유";
+
+      case "PHARMACY":
+        return "약국";
+
+      case "HOSPITAL":
+        return "병원";
+
+      case "MEDICATION":
+        return "약";
+
+      case "SYMPTOM":
+        return "증상";
+
+      case "SUPPLEMENT":
+        return "영양제";
+
+      default:
+        return category;
+    }
+  };
+
+  return { formatCategory, unformatCategory};
 };
 
 export default useFormatCategory;
--- src/pages/community/activities/components/MyCommentList.tsx
+++ src/pages/community/activities/components/MyCommentList.tsx
@@ -1,9 +1,118 @@
-import React from "react";
+import React, { Dispatch, useEffect } from "react";
 import MyCommentItem from "./MyCommentItem";
-import comments from "../../../../mocks/comments";
 import TextButton from "../../components/TextButton";
+import { axiosInstance } from "@/apis/axios-instance";
+import { keepPreviousData, useQuery, useQueryClient } from "@tanstack/react-query";
+import { CheckBoxIcon, CheckBoxOnIcon } from "@public/svgs";
+import useModalStore from "@/store/useModalStore";
+import PageNavigator from "../../components/PageNavigator";
 
-const MyCommentList = () => {
+interface Comment {
+  postId: number;
+  commentId: number;
+  title: string;
+  content: string;
+  createdAt: string;
+}
+
+interface MyList {
+  id: number;
+  isSelected: boolean;
+}
+
+interface MyCommentListProp {
+  page: number;
+  setPage: Dispatch<React.SetStateAction<number>>
+  myList: MyList[];
+  setMyList: React.Dispatch<React.SetStateAction<MyList[]>>
+  isAllSelected: boolean;
+  selectedIds: number[];
+}
+
+const MyCommentList: React.FC<MyCommentListProp> = ({
+  page = 1,
+  setPage,
+  myList = [],
+  setMyList = () => { },
+  isAllSelected,
+  selectedIds,
+}) => {
+
+  const {
+    setNoticeModalText,
+    setIsNoticeModalOpen,
+  } = useModalStore();
+
+  const queryClient = useQueryClient();
+
+  const getMyComments = async () => {
+    try {
+      const response = await axiosInstance.get(`${process.env.NEXT_PUBLIC_DOMAIN}/mypage/activities/comments`, {
+        params: {
+          page,
+        }
+      });
+      return response.data;
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  const handleCheckbox = (commentId: number) => {
+    setMyList((prev) =>
+      prev.map((item) =>
+        item.id === commentId ? { ...item, isSelected: !item.isSelected } : item
+      )
+    )
+  }
+
+  const handleAllCheckbox = () => {
+    setMyList((prev) =>
+      prev.map((item) => (
+        { ...item, isSelected: !isAllSelected }
+      ))
+    )
+  }
+
+  const handleDeleteComment = async () => {
+    if (selectedIds && selectedIds?.length > 0) {
+      try {
+        const response = await axiosInstance.patch(`${process.env.NEXT_PUBLIC_DOMAIN}/community/comments/delete`,
+          null,
+          {
+            params: {
+              commentIds: selectedIds
+            },
+            paramsSerializer: (params) => {
+              return new URLSearchParams(params).toString();
+            }
+          })
+        console.log(response)
+        queryClient.invalidateQueries({ queryKey: ['myComments', page] })
+        setNoticeModalText(`${selectedIds?.length}개의 댓글을 삭제하였습니다.`)
+        setIsNoticeModalOpen(true);
+      } catch (e) {
+        console.log(e)
+      }
+    }
+  }
+
+  const { data } = useQuery({
+    queryKey: ["myComments", page],
+    queryFn: getMyComments,
+    placeholderData: keepPreviousData,
+    retry: 0,
+  })
+
+  const commentList = data?.result?.content;
+
+  useEffect(() => {
+    setMyList(commentList?.map((comment: Comment) => ({
+      id: comment.commentId,
+      isSelected: false,
+    })))
+  }, [commentList])
+
   return (
     <div className="flex flex-col">
       <div className="max-lg:hidden flex justify-between items-center py-3 px-4 border-b border-solid border-gray-300 text-subhead1-sb text-gray-500">
@@ -15,23 +124,42 @@ const MyCommentList = () => {
         <TextButton text="댓글 편집" color="white" />
       </div>
 
-      {comments.map((comment) => {
+      {commentList?.map((comment: Comment, index: number) => {
         return (
-          <MyCommentItem
-            key={comment.id}
-            id={comment.id}
-            content={comment.content}
-            title={comment.title}
-            date={comment.date}
-          />
+          <div key={index} className={`flex gap-2`}>
+            <div onClick={() => handleCheckbox(comment.commentId)} className={`mt-3`}>
+              <CheckBoxIcon className={`w-6 mb-0.5 ${myList[index]?.isSelected ? `hidden` : ``}`} />
+              <CheckBoxOnIcon className={`w-6 mb-0.5 ${!myList[index]?.isSelected ? `hidden` : ``}`} />
+            </div>
+            <div className={`grow`}>
+              <MyCommentItem
+                key={comment.commentId}
+                postId={comment.postId}
+                commentId={comment.commentId}
+                content={comment.content}
+                title={comment.title}
+                createdAt={comment.createdAt}
+              />
+            </div>
+          </div>
         );
       })}
-      <div className="max-lg:hidden flex flex-row mt-3 justify-between items-center">
-        <div className="flex flex-row gap-2">
-          <input type="checkbox" />
-          <p className="text-subhead1-sb text-gray-300">전체 선택</p>
+      <div className={`lg:flex justify-between mt-3 hidden`}>
+        <div className={`flex gap-3 text-subhead1-sb text-gray-300`}>
+          <div onClick={handleAllCheckbox}>
+            <CheckBoxIcon className={`w-6 mb-0.5 ${isAllSelected && `hidden`}`} />
+            <CheckBoxOnIcon className={`w-6 mb-0.5 ${!isAllSelected && `hidden`}`} />
+          </div>
+          전체 선택
         </div>
-        <TextButton text="삭제" />
+        <button
+          className={`px-3 py-1 rounded text-subhead2-sb text-gray-400 bg-gray-100`}
+          onClick={handleDeleteComment}>
+          삭제
+        </button>
+      </div>
+      <div className={`lg:mt-12 mt-8`}>
+        <PageNavigator page={page} totalPage={data?.result?.totalPages} isFirst={data?.result?.first} isLast={data?.result?.last} setPage={setPage} />
       </div>
     </div>
   );
