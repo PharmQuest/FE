d2937fca012d156825b1905e7cefc5284988ed51
tellgeniewish <jadastras2@gmail.com>
2025-02-14 00:57:44 +0900

    :sparkles: 부드럽게 스크롤 추가

--- src/pages/mypage/supplement/index.tsx
+++ src/pages/mypage/supplement/index.tsx
@@ -116,6 +116,13 @@ const SupplementPage: React.FC = () => {
     setCurrentPage(1);
   };
 
+  useEffect(() => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth' // 부드러운 스크롤 효과
+    });
+  }, [currentPage]);
+
   return (
     <div className="xl:w-[900px] xl:mx-auto lg:w-[900px] lg:mx-[50px] md:w-[601px] md:mx-auto w-[calc(100%-40px)] mx-5 py-8 lg:py-9">
       <div className="flex flex-col lg:flex-row items-start lg:items-center gap-3">
