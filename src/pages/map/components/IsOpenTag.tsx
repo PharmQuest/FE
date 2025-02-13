27e6dcdd5194a3a64f8021c8dd7a14ff179b5932
hyesngy <tell280210@gmail.com>
2025-02-13 20:53:17 +0900

    🐛 영업중 태그 prop값 수정

--- src/pages/map/components/IsOpenTag.tsx
+++ src/pages/map/components/IsOpenTag.tsx
@@ -1,19 +1,25 @@
 import React from "react";
 
 interface IsOpenTagProps {
-  isOpen: boolean;
+  opening_hours?: {
+    open_now?: boolean;
+  };
 }
 
-const IsOpenTag: React.FC<IsOpenTagProps> = ({ isOpen }) => {
+const IsOpenTag: React.FC<IsOpenTagProps> = ({ opening_hours }) => {
+  if (!opening_hours?.open_now && opening_hours?.open_now !== false) {
+    return null;
+  }
+
   return (
     <div
       className={`w-fit h-[18px] flex px-1 justify-center items-center rounded shrink-0 ${
-        isOpen
+        opening_hours.open_now
           ? "bg-primary-300 text-white text-subhead3-sb"
           : "bg-primary-50 text-gray-400 text-caption1-r"
       }`}
     >
-      {isOpen ? "영업중" : "영업종료"}
+      {opening_hours.open_now ? "영업중" : "영업종료"}
     </div>
   );
 };
